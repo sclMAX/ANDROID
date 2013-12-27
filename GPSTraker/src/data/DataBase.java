d4e96b7307aa49613d32cba26f306548fe8cd6f0
sclMAX <maxisoftman@gmail.com>
2013-12-27 14:30:09 -0300

    Editando archivo de base de datos
    Trabajando en el registro de clientes para el rastreo

--- GPSTraker/src/data/DataBase.java
+++ GPSTraker/src/data/DataBase.java
@@ -13,15 +13,21 @@ public class DataBase extends SQLiteOpenHelper {
 	}
 
 	@Override
-	public void onCreate(SQLiteDatabase arg0) {
-		// TODO Auto-generated method stub
+	public void onCreate(SQLiteDatabase db) {
+		String vSQL = "";
+		vSQL = "CREATE TABLE clientes (" +
+				"id INTEGER PRIMARY KEY, " +
+				"telefono VARCHAR(20) NULL, " +
+				"nombre VARCHAR(50) NULL, " +
+				"fuc VARCHAR(20) NULL; ";
+		db.execSQL(vSQL);
 
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
-
+		String vSQL = "DROP TABLE IF EXCISTS clientes;";
+		db.execSQL(vSQL);
 	}
 
 }
